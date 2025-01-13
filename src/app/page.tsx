86e0f28f92702c88ea52f1d553375efd8635c8c2
laibajaweed <jaweedlaiba0@gmail.com>
2025-01-13 18:13:42 +0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,9 @@
-import OrderPage from "./checkout/page";
 import Hero from "./hero/page";
-import Singnup from "./join-us/page";
-import ShoppingCart from "./shoping-card/page";
 
 export default function Home() {
   return ( 
          <div>
             <Hero />
-        <OrderPage />
-          <ShoppingCart/>
-          <Singnup />
          </div>
         
          
